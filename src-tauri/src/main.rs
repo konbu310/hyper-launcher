0af8366fed7fdb771b16702bdabb1d142e5fdabc
Yuya Sato <yuyas203@gmail.com>
2023-01-13 02:34:48 +0900

    init tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::fs;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, get_config_str])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
